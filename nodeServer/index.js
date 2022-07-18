648d0f6e0e40882334c0a2a8c7ed5c41ee4c1629
Tapan Agrawal <taapanagrawal2002@gmail.com>
2022-07-18 19:50:27 +0530

    first-commit

---
+++ nodeServer/index.js
@@ -0,0 +1,18 @@
+
+//node server
+const port = (9900);
+const io = require('socket.io')(process.env.PORT || port );
+const users = {};
+
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        console.log("new user",name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive', { message: message, name: users[socket.id] })
+    });
+})
